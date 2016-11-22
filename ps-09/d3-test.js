bb10fbd5ac4c34630d6e64e32aa063add8887db5
zanerazzaq <razzaq.z@husky.neu.edu>
2016-11-22 18:16:31 -0500

    ps10

---
+++ ps-09/d3-test.js
@@ -0,0 +1,37 @@
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 4 em;
+  font-family: 'lato', sans-serif;
+  font-size: 12 pt;
+  color: hsl(20, 10%, 80%);
+  background-color: hsl(0, 0%, 85%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+svg {
+  background-color: hsl(0,10%,90%);
+  font-size: 11 pt;
+}
+
+
+.horizontal-bar {
+  width: 500 px;
+  display: flex;
+  padding: 1 em;
+  justify-content: space-around;
+  justify-self: stretch;
+  font-size: 9 pt;
+  color: hsl(20,10%,60%);
+}
+.top {
+  font-size: 10 pt;
+  font-weight: bold;
+}
+
+.footnote {
+
+}
